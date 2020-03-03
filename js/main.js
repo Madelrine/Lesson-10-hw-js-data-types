9a295509c71941bec07c99417aa95e32f96c21fb
Madelrine <1bellmerre0@gmail.com>
2020-03-03 21:42:38 +0200

    Homework javascript if else, ternary operators, typeof

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var a = 30;
+document.write("var a = 30" + " " + "is typeof" + " " + typeof a + "<br/>");
+var person = "Victor Panamarciuc";
+document.write("var person = \"Victor Panamarciuc\"" + " " + "is typeof" + " " + typeof person + "<br/>");
+var login = true;
+document.write("var login = true" + " " + "is typeof" + " " + typeof login + "<br/>");
+var name;
+document.write("var name" + " " + "is typeof" + " " + typeof name + "<br/>");
+var books = ["html", "css", "javascript"];
+document.write("var books = [\"html\", \"css\", \"javascript\"];" + " " + "is typeof" + " " + typeof books + "<br/>");
+var x = null;
+document.write("var x = null" + " " + "is typeof" + " " + typeof x + "<br/>");
+
+var myCurrentAge = 30;
+var result;
+if (myCurrentAge >= 25) {
+    document.write("<h3>I am over  25 years old</h3>")
+} else {
+    document.write("<h3>I am under 25</h3>")
+}
+var result = (myCurrentAge >= 25) ? "<h3>I am over 25 years old</h3>" : "<h3>I am under 25</h3>"
+document.write(result);
